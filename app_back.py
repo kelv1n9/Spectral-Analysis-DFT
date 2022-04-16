bcddf08d830321a151491b2d1b76e10daeeca7f8
Elvin Gadirov <gadirov190102@gmail.com>
2022-04-17 01:24:18 +0400

    Optimization of functions for the user interface.

--- app_back.py
+++ app_back.py
@@ -1,10 +1,12 @@
-from DFT import fft, ift, dft, ifft, amp_filter, ft_freq
+from DFT import fft, ift, dft, ifft, ft_freq
 from Errors import degree_error, logger
+from filters import amp_filter, butter
 import numpy as np
 import timeit
 
 """
-data = [a_ent, b_ent, f1_ent, f2_ent, amp_cut_ent, noise_amp_ent, df_ent, f_max_ent, r_btn, chk_btn, chk_btn2]
+data = [a_ent, b_ent, f1_ent, f2_ent, amp_cut_ent, freq_cut_ent, order, noise_amp_ent, 
+                                                                df_ent, f_max_ent, r_btn, chk_btn, filter_btn] 13
 """
 
 
@@ -14,11 +16,14 @@ class AppBack:
         self.b = data[1]
         self.f1 = data[2]
         self.f2 = data[3]
-        self.cutoff = data[4]
-        self.noise_amp = data[5]
-        self.df = data[6]
-        self.f_max = data[7]
-        self.radiobutton = data[8]
+        self.amp_cutoff = data[4]
+        self.freq_cutoff = data[5]
+        self.order = data[6]
+        self.noise_amp = data[7]
+        self.df = data[8]
+        self.f_max = data[9]
+        self.radiobutton = data[10]
+        self.filter_btn = data[12]
 
         self.n = int(2 * self.f_max / self.df)
         period = self.n / (2 * self.f_max)
@@ -44,7 +49,17 @@ class AppBack:
 
             start_time = timeit.default_timer()
             unfiltered_s = fft(f)  # Амплитуды
-            filtered_i = amp_filter(unfiltered_s, self.cutoff)  # Отфильтрованные Амплитуды
+
+            filtered_i = None
+
+            # Отфильтрованные Амплитуды
+            if self.filter_btn == 1:
+                filtered_i = unfiltered_s * butter(freq, self.freq_cutoff, self.order, 'low')
+            elif self.filter_btn == 2:
+                filtered_i = unfiltered_s * butter(freq, self.freq_cutoff, self.order, 'high')
+            elif self.filter_btn == 3:
+                filtered_i = amp_filter(unfiltered_s, self.amp_cutoff)
+
             filtered_s = ifft(filtered_i)  # Отфильтрованный сигнал
             time = timeit.default_timer() - start_time
 
@@ -53,10 +68,18 @@ class AppBack:
         if self.radiobutton == 2:
             start_time = timeit.default_timer()
             unfiltered_s = dft(f)  # Амплитуды
-            filtered_i = amp_filter(unfiltered_s, self.cutoff)  # Отфильтрованные Амплитуды
+            freq = ft_freq(self.n, 1. / (2 * self.f_max))  # Частоты
+
+            filtered_i = None
+
+            if self.filter_btn == 1:
+                filtered_i = unfiltered_s * butter(freq, self.freq_cutoff, self.order, 'low')
+            elif self.filter_btn == 2:
+                filtered_i = unfiltered_s * butter(freq, self.freq_cutoff, self.order, 'high')
+            elif self.filter_btn == 3:
+                filtered_i = amp_filter(unfiltered_s, self.amp_cutoff)
+
             filtered_s = ift(filtered_i)  # Отфильтрованный сигнал
             time = timeit.default_timer() - start_time
-
-            freq = ft_freq(self.n, 1. / (2 * self.f_max))  # Частоты
 
             return abs(unfiltered_s) / self.n, abs(filtered_i) / self.n, np.real(filtered_s) / self.n, freq, time
